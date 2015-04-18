c15daa6ebfda8fb845352213a0bc42fb659eee2c
Nathan Owsiany <nowsiany@gmail.com>
2015-04-18 00:54:38 -0600

    rubocop fixes...

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :responders, except: [:new, :edit, :destroy]
 
-  match "*path", to: "errors#not_found", via: :all
+  match '*path', to: 'errors#not_found', via: :all
 end
