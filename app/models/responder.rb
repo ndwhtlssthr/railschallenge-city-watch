6192d263612faad1b0ea78e7b1a81c84a72c8573
Nathan Owsiany <nowsiany@gmail.com>
2015-04-17 21:49:37 -0600

    responders create passes capacity presence validation

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,5 +1,6 @@
 class Responder < ActiveRecord::Base
   self.inheritance_column = :_type_disabled
+  validates :capacity, presence: true
   validates :capacity, inclusion: {
     in: 1..5,
     message: 'is not included in the list'
