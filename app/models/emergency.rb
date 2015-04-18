fd4bcb9ad2e1df1bf2856781a085cc1cb4bb668f
Nathan Owsiany <nowsiany@gmail.com>
2015-04-18 08:42:56 -0600

    emergency#create code uniqueness validation

--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -2,4 +2,5 @@ class Emergency < ActiveRecord::Base
   validates :fire_severity, :police_severity, :medical_severity, presence: true
   validates :fire_severity, :police_severity, :medical_severity,
             numericality: { greater_than_or_equal_to: 0 }
+  validates :code, uniqueness: true
 end
