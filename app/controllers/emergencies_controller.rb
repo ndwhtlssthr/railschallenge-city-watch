cc8440111ccc28798ccac85781975177dcac88f1
Nathan Owsiany <nowsiany@gmail.com>
2015-04-18 08:38:10 -0600

    emergencies#create validates severity numerality >= 0

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -4,7 +4,7 @@ class EmergenciesController < ApplicationController
     if @emergency.save
       render json: @emergency, status: 201
     else
-      render "error"
+      render 'error'
     end
   end
 
--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -1,2 +1,4 @@
 class Emergency < ActiveRecord::Base
+  validates :fire_severity, :police_severity, :medical_severity,
+    numericality: { greater_than_or_equal_to: 0 }
 end
