f931adb39a08a2f5f8f06529abd91ed9fcc03cc9
Nathan Owsiany <nowsiany@gmail.com>
2015-04-17 21:53:28 -0600

    responders create passes name uniqueness validation

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,5 +1,7 @@
 class Responder < ActiveRecord::Base
   self.inheritance_column = :_type_disabled
+
+  validates :name, uniqueness: true
   validates :capacity, :name, presence: true
   validates :capacity, inclusion: {
     in: 1..5,
