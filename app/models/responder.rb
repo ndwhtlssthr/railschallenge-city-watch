15223c384a652741329bdb4d36a2d661fc1a5fb5
Nathan Owsiany <nowsiany@gmail.com>
2015-04-21 21:12:33 -0600

    refactored responder class methods to scopes

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -7,25 +7,9 @@ class Responder < ActiveRecord::Base
 
   belongs_to :emergency, foreign_key: :code, primary_key: :emergency_code
 
-  def self.total_capacity(type)
-    where(type: type).sum(:capacity)
-  end
-
-  def self.not_on_response_capacity(type)
-    where(type: type).where(emergency_code: nil).sum(:capacity)
-  end
-
-  def self.on_duty_capacity(type)
-    where(type: type).where(on_duty: true).sum(:capacity)
-  end
-
-  def self.available_capacity(type)
-    where(type: type).where(emergency_code: nil).where(on_duty: true)
-      .sum(:capacity)
-  end
-
-  def self.available_units(type)
-    where(type: type).where(emergency_code: nil).where(on_duty: true)
-      .order(capacity: :desc)
-  end
+  scope :total_capacity, ->(type) { where(type: type).sum(:capacity) }
+  scope :not_on_response_capacity, ->(type) { where(type: type, emergency_code: nil).sum(:capacity) }
+  scope :on_duty_capacity, ->(type) { where(type: type, on_duty: true).sum(:capacity) }
+  scope :available_capacity, ->(type) { where(type: type, emergency_code: nil, on_duty: true).sum(:capacity) }
+  scope :available_units, ->(type) { where(type: type, emergency_code: nil, on_duty: true).order(capacity: :desc) }
 end
