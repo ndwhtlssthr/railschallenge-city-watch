bcdba9ac39a7378cba0f3fcceb1195266f392857
Nathan Owsiany <nowsiany@gmail.com>
2015-04-21 21:34:36 -0600

    change over to named status codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   rescue_from(ActionController::UnpermittedParameters) do |pme|
     render json: {
       message: "found unpermitted parameter: #{ pme.params.first }"
-    }, status: 422
+    }, status: :unprocessable_entity
   end
 
   def render_not_found
-    render json: { message: 'page not found' }, status: 404
+    render json: { message: 'page not found' }, status: :not_found
   end
 end
